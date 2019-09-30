64a948dd88e6d7b53765afc7a58f0df185d39bf1
Alejandro <alejandro.rob.alt@gmail.com>
2019-09-30 15:42:27 -0500

    Result function

--- Calculator/Controller/HomeController.swift
+++ Calculator/Controller/HomeController.swift
@@ -24,6 +24,7 @@ extension HomeViewController{
         setup()
         
         commaButton.setTitle(decimalSeparator, for: .normal)
+        result()
     }
     
     @objc func handleAcButton(sender: UIButton){
@@ -65,5 +66,47 @@ extension HomeViewController{
     @objc func handleNumbersButton(sender: UIButton){
         sender.shine()
         print(sender.tag)
+    }
+    
+    func clear(){
+        operation = .none
+        acButton.setTitle("AC", for: .normal)
+        if temp != 0{
+            temp = 0
+            operationLabel.text = "0"
+        }
+        else{
+            temp = 0
+            result()
+        }
+    }
+    
+    func result(){
+        switch operation {
+        case .none:
+            break
+        case .addition:
+            total = total + temp
+            break
+        case .substraction:
+            total = total - temp
+            break
+        case .multiplication:
+            total = total * temp
+            break
+        case .division:
+            total = total / temp
+            break
+        case .percent:
+            temp = temp / 100
+            total = temp
+            break
+        }
+        
+        if total <= maxValue || total >= minValue{
+            operationLabel.text = printFormatter.string(from: NSNumber(value: total))
+        }
+        
+        print ("Total: \(total)")
     }
 }
